7236bba344385a7089d6ad0e2de9df0bf44e8b78
Silja Ástudóttir <silja.astudottir@gmail.com>
2025-08-30 21:30:06 +0000

    Add files for ClickTriangles

---
+++ Verkefni2/clicktriangles.js
@@ -0,0 +1,4 @@
+//Þið eigið að skrifa forritið clicktriangles
+// þar sem við hvern músarsmell er teiknaður lítill þríhyrningur
+// með miðju í skjápunktinum sem smellt var á
+// (þið þurfið því að búa til 3 punkta fyrir hvern smell og setja þá í fylkið). 
